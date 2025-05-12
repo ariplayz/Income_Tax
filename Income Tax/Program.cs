c1048de83fb1937292d63d0272286ec8d9666a67
Ari Cummings <ari@arigreenecummings.com>
2025-05-12 09:40:46 -0700

    Init

---
+++ Income Tax/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Income_Tax;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int income;
+        int charity;
+        
+        Console.WriteLine("How much income did you make?");
+        income = int.Parse(Console.ReadLine());
+        Console.WriteLine("How much did you donate to charity?");
+        charity = int.Parse(Console.ReadLine());
+        
+    }
+}
